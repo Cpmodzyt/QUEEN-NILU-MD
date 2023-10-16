e8ef65d71fe51b9a7f5b617f367ac1c35e761cc7
Cpmodzyt <121564783+Cpmodzyt@users.noreply.github.com>
2023-10-16 07:27:08 +0530

    config.js

--- config.js
+++ config.js
@@ -23,15 +23,15 @@ global.POSTGRESQL_URL = ''
 
 // ⚙️ OWNER SETTINGS  ⚙️
 
-global.OWNER_NUMBER = '94767438882'
+global.OWNER_NUMBER = '94765665354'
 
-global.OWNER_NAME = 'Janiya_X'
+global.OWNER_NAME = 'I ƚ ȥ _ C ρ  |° ©'
 
 global.TIMEZONE = 'Asia/Colombo' 
 
 global.INBOX_BLOCK_MESSAGE = '' // Inboc Block Message
 
-global.INBOX_BLOCK = 'true' // TO Inbox Block On put true
+global.INBOX_BLOCK = 'false' // TO Inbox Block On put true
 
 global.READ_MESSAGE = "true" 
 
@@ -41,11 +41,11 @@ global.OPENAI_API_KEY = "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"
 ⚙️ BOT  SETTINGS  ⚙️
 */
 
-global.BOT_NAME = 'ǫᴜᴇᴇɴ ɴɪʟᴜ'
+global.BOT_NAME = '*I ᴛ ᴢ   C ᴘ ` ꜱ   B ᴏ ᴛ   </* 🤖'
  
-global.BOT_OFFLINE = false    //  Always Onlne -  | false offline -True
+global.BOT_OFFLINE = true    //  Always Onlne -  | false offline -True
 
-global.STICKER_NAME = '@Janiya 🤹‍♂️' //sticker
+global.STICKER_NAME = 'Hҽყ  Tԋҽɾҽ  I'ɱ  Cρ  ||°  🤍 🪄 🌈' //sticker
 
 global.FOOTER = 'Queen Nilu 2023 '
 
@@ -55,11 +55,41 @@ global.PREFIX = '.' // Bot Prefix
 
 global.CAPTION = 'ǫᴜᴇᴇɴ ɴɪʟᴜ  © 2023' // Caption
 
-global.ALIVELOGO = 'https://telegra.ph/file/1e63f0ee90304a12767c7.jpg' // Set Alive Logo link 
+global.ALIVELOGO = 'https://telegra.ph/file/1f2b8836893b2cd5498cb.jpg' // Set Alive Logo link 
 
-global.ALIVE_MESSAGE = 'default' // Set Alive Message
+global.ALIVE_MESSAGE = '*Hҽყ  Tԋҽɾҽ  I'ɱ  Cρ  ||°*  🤍 🪄
 
-global.MAX_SIZE = '400' // Bot Uloading Max size 
+⚡Rυɳ Tιɱҽ -  24 × 7 
+
+🔥 Sρҽҽԃ -  0.0987
+
+💠 Pʅαƚϝσɾɱ - Linux
+
+🧩 Mσԃҽ - Public 
+
+♻️ Rαɱ - 30.72 GB / 61.92 GB
+
+🪄 Vҽɾʂισɳ - v4.4.1
+
+[ Oᴡɴᴇʀ :-  𝙸𝚝𝚣_𝙲𝚙 |° ©  ]
+
+⏤͟͟͞͞  💛 ━━━ ꜱʜᴀʀᴇ ━━━ 💛  ͟͞⏤
+
+[🎭]  𝚈𝚘𝚞𝚝𝚞𝚋𝚎  - *Itz Cp*
+
+[🎯] 𝚂𝚙𝚘𝚝𝚒𝚏𝚢 - *Cpmodzyt*
+
+[🧩] 𝙳𝚒𝚜𝚌𝚘𝚛𝚍 - *Itz Cp*
+
+[🎫]  𝙶𝚛𝚘𝚞𝚙 𝙻𝚒𝚗𝚔 ;
+
+https://chat.whatsapp.com/CFvWcDUqAvK0qVS2jKKFc7
+
+#𝙸𝚝𝚣_𝙲𝚙 |° ©
+#𝚂𝚕𝚃𝚎𝚛𝚛𝚘𝚛𝙵𝚊𝚖𝚒𝚕𝚢
+#𝙰𝚙𝚙𝚕𝚒𝚌𝚊𝚝𝚒𝚘𝚗𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛' // Set Alive Message
+
+global.MAX_SIZE = '1000' // Bot Uloading Max size 
 
 global.ANTI_BAD = 'true' // Antibad (on = True , Off = False )
 
@@ -71,25 +101,25 @@ global.ANTI_BOTS = true
 
 global.ONLY_GROUP = 'false' // Only group mode
 
-global.GROUP_LINK = 'https://chat.whatsapp.com/IRdyJY5P4sF5BhtP5PIdHH' // ur group link
+global.GROUP_LINK = 'https://chat.whatsapp.com/CFvWcDUqAvK0qVS2jKKFc7' // ur group link
 
 
 /* 18 + Download Settings */
 
-global.SEX_DL = 'false' // 18 + video dl
+global.SEX_DL = 'true' // 18 + video dl
 
 /* AUTO REPLY SETTINGS */
 
 global.AUTO_STICKER = true
 
-global.AUTO_REPLY = true
+global.AUTO_REPLY = false
 
-global.VOICE_REPLY = true
+global.VOICE_REPLY = false
 
 
 /* V card */
 
-global.ytname = "YT: janithsadanuwan" //ur yt chanel name
-global.socialm = "GitHub: janithsadanuwan" //ur github or insta name
+global.ytname = "YT: *Itz Cp*" //ur yt chanel name
+global.socialm = "GitHub: *Cpmodzyt*" //ur github or insta name
 global.location = "Srilanka, colombo" //ur location
 
